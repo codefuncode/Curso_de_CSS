56052cce8e1cc3e6187dbbd12b25c5b4d55570d5
codefuncode <codefuncode@protonmail.com>
2020-11-29 22:32:45 -0400

    SEguiminto cajas de fonface  primera caja de controles

--- 01-Tutorial-CSS/015-Fuentes-CSS/Propiedades/js.js
+++ 01-Tutorial-CSS/015-Fuentes-CSS/Propiedades/js.js
@@ -14,7 +14,11 @@ var
     document.querySelectorAll('.display'),
     option_font_face = [],
     option_css_fonts_guide = [],
-    option_css_fonts = [];
+    option_css_fonts = [],
+    control_en_curso,
+    head = document.head;
+
+// console.log(head);
 // ======================================================
 
 // ===============================================
@@ -29,38 +33,13 @@ for (var i = 0; i < input_radio.length; i++) {
     });
 }
 
-for (var i = 0; i < select.length; i++) {
-
-    if (i < 3) {
-        console.log(select[i]);
-
-    }
-
-}
-
-// ===============================================
-
-// =========================================================
-//  test conteo de elementos 
-// console.log('===========select=============');
-// console.log(select.length);
-// console.log('===========controles=============');
-// console.log(controles.length);
-// console.log('===========type="radio"=============');
-// console.log(input_radio.length);
-// console.log('========================');
-// =========================================================
-
 // ===========================================================
-//  Aplica como valor eel valor dentro de la etiqueta option correspondiente 
+//  Aplica como valor el valor dentro de la etiqueta option correspondiente 
 for (var i = 0; i < select.length; i++) {
 
     select[i].value = "";
-    // console.log(i);
 
     for (var j = 0; j < select[i].children.length; j++) {
-
-        // console.log('============================');
 
         // console.log(select[i].children[j]);
 
@@ -109,36 +88,21 @@ for (var i = 0; i < select.length; i++) {
     }
 
 }
-// console.log('===================');
-// for (var i = 0; i < option_font_face.length; i++) {
-//     console.log(option_font_face[i]);
-// }
-// console.log('===================');
 
-// for (var i = 0; i < option_css_fonts_guide.length; i++) {
-//     console.log(option_css_fonts_guide[i]);
-// }
-// console.log('===================');
-
-// for (var i = 0; i < option_css_fonts.length; i++) {
-//     console.log(option_css_fonts[i]);
-// }
-// console.log('===================');
-// ===========================================================
-
-// for (var i = 0; i < controles.length; i++) {
-//     console.log(controles[i]);
-// }
-
-var opciones_todas =
+var
+    opciones_todas =
     option_font_face.concat(
-        option_css_fonts_guide, option_css_fonts);
+        option_css_fonts_guide,
+        option_css_fonts
+    );
+
 console.log('===========   Todas las opciones   ===============');
 for (var i = 0; i < opciones_todas.length; i++) {
 
     console.log(opciones_todas[i]);
 
 }
+
 console.log('===========   Todas las opciones   ===============');
 console.log(controles.length + " " + "controles");
 console.log(opciones_todas.length + " " + "opciones ");
@@ -147,74 +111,67 @@ console.log(opciones_todas.length + " " + "opciones ");
 //  Mostrar caja de controles corespondiente 
 for (var i = 0; i < select.length; i++) {
 
-    select[i].addEventListener('input', function(argument) {
+    select[i].addEventListener('input', selecciona_caja_control);
 
-        // console.log('=============');
-        console.log(this);
-        console.log(this.value);
+}
 
-        for (var i = 0; i < select.length; i++) {
+function selecciona_caja_control(argument) {
+    // console.log('=============');
+    console.log(this);
+    console.log(this.value);
 
-            if ((this == select[0] || this == select[1] || this == select[2])) {
+    for (var i = 0; i < select.length; i++) {
 
-                if (this != select[i]) {
-
-                    select[i].value = "";
-
-                }
-            }
-
-        }
         if ((this == select[0] || this == select[1] || this == select[2])) {
 
-            for (var i = 0; i < controles.length; i++) {
+            if (this != select[i]) {
 
-                if (i == opciones_todas.indexOf(this.value)) {
+                select[i].value = "";
 
-                    controles[i].style.display = "block";
-                } else {
-
-                    controles[i].style.display = "none";
-                }
             }
-
         }
 
-        // console.log(this.children.length);
-        // console.log('=============');
+    }
 
-        // if ((this.getAttribute("id")) == "1") {
-        //     console.log("@font_face: attr");
+    if ((this == select[0] || this == select[1] || this == select[2])) {
 
-        //     for (var i = 0; i < controles.length; i++) {
+        for (var i = 0; i < controles.length; i++) {
 
-        //         console.log(controles[i]);
+            if (i == opciones_todas.indexOf(this.value)) {
 
-        //         if (i == 0) {
+                controles[i].style.display = "block";
 
-        //             controles[i].style.display = "block";
-        //         } else {
-        //             controles[i].style.display = "none";
-        //         }
+                control_en_curso = controles[i];
 
-        //     }
-        // }
+            } else {
 
-    });
+                controles[i].style.display = "none";
+            }
+        }
+
+    }
+
+    // =======================================================
+    console.log("Control en curso = ");
+    console.log(control_en_curso);
+    var matrizx = [];
+    var x = control_en_curso.childNodes;
+
+    for (var i = 0; i < x.length; i++) {
+
+        if (x[i].nodeName == "SELECT") {
+
+            matrizx.push(x[i]);
+
+        } else {
+
+            // console.log(x[i]);
+        }
+
+    }
+
+    console.log(matrizx);
+
+    // =======================================================
 }
 // ===================================================
-
-// =======================================
-//  Prueba para definir el valor de los elementos option 
-// console.log(select[0].children);
-// console.log(select[0].children.length);
-// console.log(select[0].children[0]);
-// console.log(select[0].children[1]);
-// console.log(select[0].children[2]);
-// console.log(select[0].children[3]);
-// console.log(select[0].children[4]);
-// console.log(select[0].children[5]);
-// console.log(select[0].children[6]);
-// console.log(select[1].children);
-// console.log(select[2].children);
-// =======================================
